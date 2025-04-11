49944eacae8fb1c5341bb4d06aedbb2129a61d6a
Luke Liu <3359767@gmail.com>
2025-04-12 06:12:15 +0800

    Delete cursor.js

--- cursor.js
+++
@@ -1,9 +0,0 @@
-const cursor = document.getElementById('cursor');
-document.addEventListener('mousemove', e => {
-    cursor.style.left = `${e.pageX}px`;
-    cursor.style.top = `${e.pageY}px`;
-});
-document.querySelectorAll('a, button').forEach(el => {
-    el.addEventListener('mouseenter', () => cursor.classList.add('hover'));
-    el.addEventListener('mouseleave', () => cursor.classList.remove('hover'));
-});
